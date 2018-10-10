e00c42a35fa54e1dd38d7d0727b27c0e788ab83a
jiayu <jiayuchen233@gmail.com>
2018-10-10 19:33:14 +0200

    add first version

---
+++ saliency_map_leonard.py
@@ -0,0 +1,177 @@
+import argparse,sys
+
+import tensorflow as tf
+import numpy as np
+import PIL
+
+from utils import preprocess_img,classify,load_imagenet_label,calculate_img_region_importance,calculate_region_importance,load_pretrain_model,model_train,show_gradient_map
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", help="specify the pretrained model name", required=False, default='vgg16')
+    parser.add_argument("--img_path", help='specify the image path', required=False, default='./picture/cat.jpg')
+    parser.add_argument("--true_label", type=int, help='the groundtruth class for input image', required=False, default=281)
+    parser.add_argument("--adv_label", type=int, help='specify the target adversarial label', default=100)
+    parser.add_argument("--epoch", type=int, help="specify the training epochs", default=100)
+    parser.add_argument("--eps", type=float, help="specify the degree of disturb", default=8/255)
+    parser.add_argument("--lr", type=float, help="specify the learning rate", default=2)
+    parser.add_argument("--lambda_up", type=float, help="specify the lambda for increasing gradient in the given region", default=10)
+    parser.add_argument("--lambda_down", type=float, help="specify the lambda for lowering gradient in the given region", default=10)
+    parser.add_argument("--label_num", type=int, help="specify the number of final outputs in pretrained model", default=1000)
+    parser.add_argument("--is_cluster", help="indicate if work on cluster so that graphic function is not available", action="store_true", required=False, default=False)
+    parser.add_argument("--summary_path", help="specify the tensorflow summary file path", default='./summary')
+    pargs = parser.parse_args(argv)
+    return pargs
+
+
+def main(args):
+    for arg in vars(args):
+        print(arg, getattr(args, arg))
+
+    model_name = args.model_name
+    img_path = args.img_path
+    img_label_path = 'imagenet.json'
+    true_class = args.true_label
+    adversarial_label = args.adv_label
+    demo_epoch = args.epoch
+    demo_eps = args.eps
+    demo_lr = args.lr
+    label_num = args.label_num
+    lambda_up, lambda_down = args.lambda_up, args.lambda_down
+
+    # load model
+    sess, graph, img_size, images_v, logits = load_pretrain_model(model_name)
+    summary_writer = tf.summary.FileWriter(args.summary_path, graph)
+    print("sucessfully load model")
+    probs = tf.nn.softmax(logits)
+
+    y_hat = tf.placeholder(tf.int32,())
+    label_logits = tf.gather_nd(logits, [[0, y_hat]])
+    global_step = tf.Variable(0, name="global_step", trainable=False)
+    step_init = tf.variables_initializer([global_step])
+
+    img = PIL.Image.open(img_path)
+    img = preprocess_img(img, img_size)
+    batch_img = np.expand_dims(img,0)
+    imagenet_label = load_imagenet_label(img_label_path)
+
+    # -------------------
+    # Step 1: classify the image with original model
+    p = sess.run(probs, feed_dict={images_v: batch_img})[0]
+    predict_label = np.argmax(p)
+    classify(img, p, imagenet_label, correct_class=true_class, is_cluster=args.is_cluster)
+
+    # -------------------
+    # Step 2: Construct adversarial examples
+    image_pl = tf.placeholder(tf.float32, (1, img_size, img_size, 3))
+    assign_op = tf.assign(images_v, image_pl)
+    learning_rate = tf.placeholder(tf.float32, ())
+    var_eps = tf.placeholder(tf.float32,())
+    labels = tf.one_hot(y_hat, label_num)
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)[0]
+
+    projected = tf.clip_by_value((tf.clip_by_value(images_v, image_pl - var_eps, image_pl + var_eps)), 0, 1)
+    with tf.control_dependencies([projected]):
+        project_step = tf.assign(images_v, projected)
+
+    # initialization step
+    _ = sess.run([assign_op, step_init], feed_dict={image_pl: batch_img})
+
+    # construct targeted attack
+    # feed_dict_optim = {image_pl:batch_img,
+    #                    y_hat:adversarial_label,
+    #                    learning_rate:demo_lr}
+    #
+    # feed_dict_proj = {image_pl:batch_img,
+    #                   var_eps:demo_eps}
+    # optim_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, var_list=[images_v])
+    # model_train(sess=sess,
+    #             optim_step=optim_step,
+    #             project_step=project_step,
+    #             loss=loss,
+    #             feed_dict_optim=feed_dict_optim,
+    #             feed_dict_project=feed_dict_proj,
+    #             epoch=10)
+    #
+    # adv_img = np.squeeze(images_v.eval(),0)
+    # adv_prob = sess.run(probs,feed_dict={images_v:np.expand_dims(adv_img,0)})
+    # classify(adv_img, adv_prob[0],imagenet_label,correct_class=281,target_class=adversarial_label)
+    #
+    # # show the saliency map
+    # feed_dict_gradient = {y_hat:true_class}
+    # _ = show_gradient_map(graph=graph,
+    #                   sess=sess,
+    #                   y=label_logits,
+    #                   x=images_v,
+    #                   img=img,
+    #                   is_integrated=False,
+    #                   is_smooth=False,
+    #                   feed_dict=feed_dict_gradient)
+    #---------------
+    # use gradient descent to control the saliency map
+
+    # original gradient intensity
+    map3D, map_grey = show_gradient_map(graph=graph,
+                      sess=sess,
+                      y=label_logits,
+                      x=images_v,
+                      img=img,
+                      is_integrated=False,
+                      is_smooth=True,
+                      feed_dict={y_hat:true_class},
+                      is_cluster=args.is_cluster)
+
+    center_more, radius_more = (50, 100), 10
+    center_less, radius_less = (100, 50), 10
+    gradient_more = calculate_region_importance(map_grey,center_more, radius_more)
+    gradient_less = calculate_region_importance(map_grey,center_less, radius_less)
+    print("region 1 gradient intensity %.3f, region 2 gradient intensity %.3f" % (gradient_more, gradient_less))
+
+    # construct new loss function
+    grad_map = tf.gradients(label_logits,images_v)[0]
+    up_gradient = calculate_img_region_importance(grad_map, center_more, radius_more)
+    down_gradient = calculate_img_region_importance(grad_map, center_less, radius_less)
+    grad_loss = -lambda_up*up_gradient + lambda_down*down_gradient
+    final_loss = grad_loss+loss
+    up_gradient_summary = tf.summary.scalar("up_gradient", up_gradient)
+    down_gradient_summary = tf.summary.scalar("down_gradient", down_gradient)
+    loss_summary = tf.summary.scalar("loss", loss)
+    train_summary_op = tf.summary.merge_all()
+    change_grad_optim_step = tf.train.GradientDescentOptimizer(learning_rate=demo_lr).minimize(final_loss,var_list=[images_v],global_step=global_step)
+    for i in range(demo_epoch):
+        _,_loss,step,summary_str = sess.run([change_grad_optim_step, final_loss, global_step,train_summary_op],
+                                                  feed_dict={image_pl:batch_img,y_hat:true_class,learning_rate:demo_lr})
+        summary_writer.add_summary(summary_str,global_step=step)
+        sess.run([project_step], feed_dict={image_pl:batch_img,var_eps:demo_eps})
+        print("%d loss = %g" % (i,_loss))
+
+    adv_img = np.squeeze(images_v.eval(), 0)
+
+    # check the prediction result
+    p_adv = sess.run(probs,feed_dict={images_v: batch_img})[0]
+    predict_label_adv = np.argmax(p_adv)
+    classify(adv_img, p_adv, imagenet_label, correct_class=true_class,is_cluster=args.is_cluster)
+
+    # check the gradient map
+    map3D_adv, map_grey_adv = show_gradient_map(graph=graph,
+                      sess=sess,
+                      y=label_logits,
+                      x=images_v,
+                      img=adv_img,
+                      is_integrated=False,
+                      is_smooth=True,
+                      feed_dict={y_hat:true_class},
+                      is_cluster=args.is_cluster)
+
+    adv_gradient_more = calculate_region_importance(map_grey_adv, center_more, radius_more)
+    adv_gradient_less = calculate_region_importance(map_grey_adv, center_less, radius_less)
+    for arg in vars(args):
+        print(arg, getattr(args, arg))
+    print("Adversarial Case: predict label: %d, region 1 gradient intensity: %.3f, region 2 gradient intensity: %.3f" % (predict_label_adv, adv_gradient_more, adv_gradient_less))
+    print("Normal Case: predict label: %d, region 1 gradient intensity: %.3f, region 2 gradient intensity: %.3f" % (predict_label, gradient_more, gradient_less))
+
+
+if __name__ == "__main__":
+    args = parse_arguments(sys.argv[1:])
+    main(args)
---
+++ utils.py
@@ -0,0 +1,177 @@
+from urllib.request import urlretrieve
+import tarfile
+import os
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import tensorflow as tf
+import tensorflow.contrib.slim.nets as nets
+import tensorflow.contrib.slim as slim
+import saliency
+
+def maybe_download_and_extract(dest_directory,data_url):
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+    filename = data_url.split('/')[-1]
+    filepath = os.path.join(dest_directory,filename)
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+                filename, float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+        filepath, _ = urlretrieve(data_url, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+        tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+
+def classify(img, p, imagenet_labels, correct_class=None, target_class=None, is_cluster=False):
+    if not is_cluster:
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 8))
+        fig.sca(ax1)
+        ax1.imshow(img)
+        fig.sca(ax1)
+        topk = list(p.argsort()[-10:][::-1])
+        topprobs = p[topk]
+        barlist = ax2.bar(range(10), topprobs)
+        if target_class in topk:
+            barlist[topk.index(target_class)].set_color('r')
+        if correct_class in topk:
+            barlist[topk.index(correct_class)].set_color('g')
+        plt.sca(ax2)
+        plt.ylim([0, 1.1])
+        plt.xticks(range(10),
+                   [imagenet_labels[i][:15] for i in topk],
+                   rotation='vertical')
+        fig.subplots_adjust(bottom=0.2)
+        plt.show()
+
+
+def preprocess_img(img,target):
+    wide_is_bigger = img.width > img.height
+    new_w = target if not wide_is_bigger else int(img.width * target / img.height)
+    new_h = target if wide_is_bigger else int(img.height * target / img.width)
+    new_img = img.resize((new_w, new_h)).crop((0, 0, target, target))
+    new_img = (np.asarray(new_img) / 255.0).astype(np.float32)
+    return new_img
+
+
+def load_imagenet_label(file_path):
+    with open(file_path) as f:
+        imagenet_labels = json.load(f)
+    return imagenet_labels
+
+
+def ShowGrayscaleImage(im, title='', ax=None):
+    if ax is None:
+        plt.figure()
+        plt.axis('off')
+    plt.imshow(im, cmap=plt.cm.gray, vmin=0, vmax=1)
+    plt.title(title)
+
+
+def calculate_region_importance(greymap,center,radius=10):
+    length, width = greymap.shape
+    left_bound = max(center[1]-radius,0)
+    right_bound = min(center[1]+radius,width)
+    up_bound = max(center[0]-radius,0)
+    lower_bound = min(center[0]+radius,length)
+    importance = np.sum(abs(greymap[left_bound:right_bound,up_bound:lower_bound]))
+    return importance
+
+
+def calculate_img_region_importance(map3D,center,radius=10):
+    batch_num, length, width, channel = map3D.shape
+    assert channel == 3
+    assert batch_num == 1
+    greymap = tf.squeeze(map3D,0)
+    greymap = tf.reduce_sum(tf.abs(greymap),2)
+    left_bound = max(center[1]-radius,0)
+    right_bound = min(center[1]+radius,width)
+    up_bound = max(center[0]-radius,0)
+    lower_bound = min(center[0]+radius,length)
+    importance = tf.reduce_sum(greymap[left_bound:right_bound,up_bound:lower_bound])
+    return importance
+
+
+def load_pretrain_model(model_name='vgg16'):
+    sess = tf.InteractiveSession()
+    if model_name == 'vgg16':
+        img_size = 224
+        images_v = tf.Variable(tf.zeros((1, img_size, img_size, 3)))
+        preprocessed = tf.multiply(tf.subtract(images_v, 0.5), 2.0)
+        arg_scope = nets.vgg.vgg_arg_scope(weight_decay=0.0)
+        with slim.arg_scope(arg_scope):
+            logits, _ = nets.vgg.vgg_16(
+                preprocessed, num_classes=1000, is_training=False)
+
+        # restore model
+        data_url = 'http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz'
+        dest_directory = './imagenet'
+        maybe_download_and_extract(dest_directory, data_url)
+        restore_vars = [
+            var for var in tf.global_variables()
+            if var.name.startswith('vgg_16/')
+        ]
+        saver = tf.train.Saver(restore_vars)
+        saver.restore(sess, os.path.join(dest_directory, 'vgg_16.ckpt'))
+        graph = tf.get_default_graph()
+
+    return sess, graph, img_size, images_v, logits
+
+
+def show_gradient_map(graph, sess, y, x, img,
+                      is_integrated=False, is_smooth=True, feed_dict=None, is_cluster=False):
+
+    if not is_integrated and not is_smooth:
+        gradient_saliency = saliency.GradientSaliency(graph, sess, y, x)
+        vanilla_mask_3d = gradient_saliency.GetMask(img, feed_dict=feed_dict)
+        vanilla_mask_grayscale = saliency.VisualizeImageGrayscale(vanilla_mask_3d)
+        if not is_cluster:
+            ShowGrayscaleImage(vanilla_mask_grayscale, title='Vanilla Gradient')
+        return vanilla_mask_3d, vanilla_mask_grayscale
+
+    if not is_integrated and is_smooth:
+        gradient_saliency = saliency.GradientSaliency(graph, sess, y, x)
+        smoothgrad_mask_3d = gradient_saliency.GetSmoothedMask(img, feed_dict=feed_dict)
+        smoothgrad_mask_grayscale = saliency.VisualizeImageGrayscale(smoothgrad_mask_3d)
+        if not is_cluster:
+            ShowGrayscaleImage(smoothgrad_mask_grayscale, title='SmoothGrad')
+        return smoothgrad_mask_3d, smoothgrad_mask_grayscale
+
+    if is_integrated and not is_smooth:
+        baseline = np.zeros(img.shape)
+        baseline.fill(-1)
+        gradient_saliency = saliency.IntegratedGradients(graph, sess, y, x)
+        vanilla_mask_3d = gradient_saliency.GetMask(img, feed_dict=feed_dict, x_steps=5, x_baseline=baseline)
+        vanilla_mask_grayscale = saliency.VisualizeImageGrayscale(vanilla_mask_3d)
+        if not is_cluster:
+            ShowGrayscaleImage(vanilla_mask_grayscale, title='Vanilla Gradient')
+        return vanilla_mask_3d, vanilla_mask_grayscale
+
+    if is_integrated and is_smooth:
+        baseline = np.zeros(img.shape)
+        baseline.fill(-1)
+        gradient_saliency = saliency.IntegratedGradients(graph, sess, y, x)
+        smoothgrad_mask_3d = gradient_saliency.GetSmoothedMask(img, feed_dict=feed_dict, x_steps=5, x_baseline=baseline)
+        smoothgrad_mask_grayscale = saliency.VisualizeImageGrayscale(smoothgrad_mask_3d)
+        if not is_cluster:
+            ShowGrayscaleImage(smoothgrad_mask_grayscale, title='SmoothGrad')
+        return smoothgrad_mask_3d, smoothgrad_mask_grayscale
+
+
+def model_train(sess, optim_step, project_step, loss,
+                feed_dict_optim, feed_dict_project, epoch=100):
+    for i in range(epoch):
+        _, _loss = sess.run([optim_step, loss],
+                            feed_dict=feed_dict_optim)
+        sess.run([project_step], feed_dict=feed_dict_project)
+        print("epoch %d , loss=%g" % (i, _loss))
+
+
+
+
+
+
