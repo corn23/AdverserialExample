fix gradient computing and add N to parser

saliency_map.py
@@ -24,6 +24,7 @@ def parse_arguments(argv):
     parser.add_argument("--summary_path", help="specify the tensorflow summary file path", default='./summary')
     parser.add_argument("--write_summary", help="write summary or not", action="store_true", default=False)
     parser.add_argument("--image_interval", type=int, help="write image into summary every *image_interval*",default=5)
+    parser.add_argument("-N", type=int,help="specify the number vectors in NES", default=30)
     pargs = parser.parse_args(argv)
     return pargs
 
@@ -39,10 +40,10 @@ def main(args):
     label_num = args.label_num
     lambda_up, lambda_down, lambda_label_loss = args.lambda_up, args.lambda_down,args.lambda_label_loss
 
-    model_name = 'inception_v3'
-    img_path = './picture/dog_cat.jpg'
-    img_label_path = 'imagenet.json'
-    true_class = 208
+    # model_name = 'inception_v3'
+    # img_path = './picture/dog_cat.jpg'
+    # img_label_path = 'imagenet.json'
+    # true_class = 208
     sess, graph, img_size, images_pl, logits = load_pretrain_model(model_name)
     y_label = tf.placeholder(dtype=tf.int32,shape=())
     label_logits = logits[0,y_label]
@@ -111,14 +112,14 @@ def main(args):
     #     epoch -= 1
 
     # try NES (Natural evolutionary strategies)
-    N = 30
+    N = args.N
     sigma = 0.001
-    eta = 0.0001
     epsilon = args.eps
     epoch = args.epoch
+    eta = args.lr
     loss = -lambda_up*to_inc_region+lambda_down*to_dec_region
-    img = np.array(old_img)
-    old_loss = sess.run(loss,feed_dict={images_pl: np.expand_dims(old_img, 0), y_label: true_class})
+    old_loss = sess.run(loss,feed_dict={images_pl: np.expand_dims(img, 0), y_label: true_class})
+    #eta = 0.01/abs(old_loss)
     num_list = '_'.join([model_name,str(to_dec_center[0]),str(to_dec_center[1]),str(to_dec_radius[0]),str(to_dec_radius[1]),
                          str(N),str(eta),str(epoch),str(lambda_down),str(lambda_up)])
     print(num_list)

utils.py
@@ -91,7 +91,7 @@ def calculate_img_region_importance(map3D,center,radius=(10,10)):
     assert channel == 3
     assert batch_num == 1
     greymap = tf.squeeze(map3D,0)
-    greymap = tf.reduce_sum(tf.abs(greymap),2)
+    greymap = tf.reduce_mean(tf.abs(greymap),2)
     up_bound = max(center[0]-radius[0],0)
     lower_bound = min(center[0]+radius[0],length)
     left_bound = max(center[1]-radius[1],0)